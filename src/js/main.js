66d710b936a0debbb50164f9e76b9a46db8b7869
Nataly <ncgarzon88@gmail.com>
2018-05-30 23:24:37 -0400

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+class TodoList {
+	constructor() {
+		
+	}
+}
+
+
+
+class TodoItem {
+	constructor(text) {
+
+	}
+}
+
+
+let todoList = new TodoList();
+let todoItem1 = new TodoItem(`Milk`);
+let todoItem2 = new TodoItem(`Eggs`);
+let todoItem3 = new TodoItem(`Diapers`);
